3c59fd9e955a026f07298260d7dc78b50708f86a
hiroki-Yim <imhk_@naver.com>
2019-11-01 15:31:44 +0900

    docs: Edit config Include New Feature

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'user', 'middleware'=>'auth.jwt'], function () use ($router){    
+
+    $router->post('/signup/{provider?}', 'UserController@signup');  # 회원가입
+    $router->post('/signin/{provider?}', 'UserController@signin');  # 로그인
+    $router->get('/logout', 'UserController@logout');               # 로그아웃
+    $router->get('/{uuid?}', 'UserController@info');                # 유저정보
+    $router->put('/{uuid?}', 'UserController@update');              # 유저정보 수정
+    $router->delete('/signout', 'UserController@signout');          # 회원 탈퇴
+
+});
